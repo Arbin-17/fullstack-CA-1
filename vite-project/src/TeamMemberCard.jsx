7f1c6c5ddeacbba89d21ef4bfe6dd62fadf079f7
Arbin-17 <arbinkumar015@gmail.com>
2025-02-24 15:44:18 +0530

    first commit

---
+++ vite-project/src/TeamMemberCard.jsx
@@ -0,0 +1,13 @@
+// src/components/TeamMemberCard.jsx
+import React from "react";
+
+const TeamMemberCard = ({ name, jobTitle }) => {
+  return (
+    <div style={{ border: "1px solid #ccc", padding: "10px", margin: "10px", borderRadius: "5px" }}>
+      <h2>{name}</h2>
+      <p>{jobTitle}</p>
+    </div>
+  );
+};
+
+export default TeamMemberCard;
